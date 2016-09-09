b996a70fa7a1bc9636ba26935f09c0505de58cd1
DanielCollins <danielcollins@csiro.au>
2016-09-09 07:39:50 +0000

    ASKAPSDP-2151 Refactored mock ingest tests to use the new functional test base class

--- Code/Components/Services/manager/current/functests/test_sbstate_changed/test_sbstate_changed.py
+++ Code/Components/Services/manager/current/functests/test_sbstate_changed/test_sbstate_changed.py
@@ -9,103 +9,16 @@
 """
 
 import os
-import sys
-import threading
 from datetime import datetime
 from time import sleep
 
 from unittest import skip
 
-import Ice, IceStorm
-from askap.iceutils import IceSession, get_service_object, Server
+import IceStorm
+from askap.iceutils import CPFuncTestBase, get_service_object
 from askap.slice import CP, SchedulingBlockService
-from askap.interfaces.cp import ICPObsServicePrx, ICPFuncTestReporter
+from askap.interfaces.cp import ICPObsServicePrx
 from askap.interfaces.schedblock import ISBStateMonitorPrx, ObsState
-
-
-class FeedbackService(ICPFuncTestReporter):
-    """ Service implementation of the askap.interfaces.cp.ICPFuncTestReporter
-    Ice interface.
-
-    Used for feedback from processes under test to the test driver.
-    """
-    def __init__(self):
-        self.history = []
-        """The method call history. Each entry is a (str, dict) tuple. The str
-        gives the method name, and the dict gives the method args as name:value
-        pairs.
-        """
-
-    def clear_history(self):
-        """ Clears the history list. """
-        self.history = []
-
-    def sbStateChangedNotification(self, sbid, obsState, current=None):
-        # print >> sys.stderr, '\nSB {0} state changed: {1}'.format(sbid, obsState)
-        self.history.append(
-            ('sbStateChangedNotification', {
-                'sbid': sbid,
-                'obsState': obsState,
-            }))
-
-
-class FuncTestServer(Server):
-    """ Ice Server for hosting the FeedbackService. """
-    def __init__(self, feedback_service, comm):
-        self.feedback_service = feedback_service
-        super(FuncTestServer, self).__init__(
-            comm,
-            configurable=False,
-            retries=10,
-            monitoring=False)
-
-    def initialize_services(self):
-        self.add_service('FuncTestReporter', self.feedback_service)
-
-    def wait_async(self):
-        def worker():
-            # print >> sys.stderr, 'FuncTestServer thread waiting'
-            self.wait()
-
-        t = threading.Thread(target=worker)
-        t.start()
-        return t
-
-
-class CPFuncTestBase(object):
-    def __init__(self):
-        self.ice_session = None
-        self.server = None
-        self.server_thread = None
-        self.feedback_service = None
-
-    def setUp(self):
-        self.ice_session = IceSession(
-            os.path.expandvars("$TEST_DIR/applications.txt"),
-            cleanup=True)
-        try:
-            self.ice_session.start()
-            self.setup_feedback_service()
-        except Exception as ex:
-            self.ice_session.communicator.destroy()
-            self.ice_session.terminate()
-            raise
-
-    def tearDown(self):
-        self.shutdown()
-
-    def shutdown(self):
-        self.ice_session.communicator.destroy()
-        self.ice_session.terminate()
-        self.ice_session = None
-
-    def setup_feedback_service(self):
-        self.feedback_service = FeedbackService()
-        self.server = FuncTestServer(
-            self.feedback_service,
-            self.ice_session.communicator)
-        self.server.setup_services()
-        # self.server_thread = self.server.wait_async()
 
 
 class TestSBStateChanged(CPFuncTestBase):
@@ -120,7 +33,6 @@ class TestSBStateChanged(CPFuncTestBase):
         # relative to that location.
         os.environ["ICE_CONFIG"] = "config-files/ice.cfg"
         os.environ['TEST_DIR'] = 'test_sbstate_changed'
-
         super(TestSBStateChanged, self).setUp()
 
         try:
