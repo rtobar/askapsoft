650cf41b21bd97c5f3d2f3c4c207a3847050ec78
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2016-09-26 12:06:52 +1000

    basic app framework that builds

--- Code/Components/CP/linmos-mpi/current/apps/linmos-mpi.cc
+++ Code/Components/CP/linmos-mpi/current/apps/linmos-mpi.cc
@@ -1,14 +1,52 @@
 
-#include<linmosMPI/linmosMPIApp.h>
+///local includes
+
+///ASKAP includes
+#include <askap/Application.h>
+#include <askap/AskapLogging.h>
+#include <askap/AskapError.h>
+#include <askap/StatReporter.h>
+
+///CASA includes
+#include <Common/ParameterSet.h>
+
 
 ASKAP_LOGGER(logger, ".linmos");
 
-using namespace casa;
+
 using namespace askap;
 
 
+class linmosMPIApp : public askap::Application
+{
+    public:
+
+        virtual int run(int argc, char* argv[]) {
+
+            try {
+                StatReporter stats;
+                LOFAR::ParameterSet subset(config().makeSubset("linmos."));
+                stats.logSummary();
+                return 0;
+            }
+            catch (const askap::AskapError& e) {
+                ASKAPLOG_FATAL_STR(logger, "Askap error in " << argv[0] << ": " << e.what());
+                std::cerr << "Askap error in " << argv[0] << ": " << e.what() << std::endl;
+                return 1;
+            } catch (const std::exception& e) {
+                ASKAPLOG_FATAL_STR(logger, "Unexpected exception in " << argv[0] << ": " << e.what());
+                std::cerr << "Unexpected exception in " << argv[0] << ": " << e.what()
+                    << std::endl;
+                return 1;
+            }
+
+
+    };
+
+};
+
 int main(int argc, char *argv[])
 {
-    LinmosMPIApp app;
+    linmosMPIApp app;
     return app.main(argc, argv);
 }
