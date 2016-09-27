5611d053dbbc35600603a21eca5dc670fa17c50e
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2016-09-27 11:46:43 +1000

    Simple framework that can pull in slices from the input images based on rank()

--- Code/Components/Synthesis/synthesis/current/apps/linmos-mpi.cc
+++ Code/Components/Synthesis/synthesis/current/apps/linmos-mpi.cc
@@ -1,14 +1,21 @@
 
 ///local includes
 #include <askap_synthesis.h>
-#include <utils/linmosUtils.h>
+#include <utils/LinmosUtils.h>
 #include <measurementequation/SynthesisParamsHelper.h>
 ///ASKAP includes
 #include <askap/Application.h>
 #include <askap/AskapLogging.h>
 #include <askap/AskapError.h>
 #include <askap/StatReporter.h>
+#include <askapparallel/AskapParallel.h>
+
 ///CASA includes
+#include <casacore/images/Images/ImageInterface.h>
+#include <casacore/images/Images/PagedImage.h>
+#include <casacore/images/Images/SubImage.h>
+
+///3rd party
 #include <Common/ParameterSet.h>
 
 
@@ -21,13 +28,183 @@ using namespace askap::synthesis;
 
 // @brief do the merge
 /// @param[in] parset subset with parameters
-static void mergeMPI(const LOFAR::ParameterSet &parset) {
+static void mergeMPI(const LOFAR::ParameterSet &parset, askap::askapparallel::AskapParallel &comms) {
 
     ASKAPLOG_INFO_STR(logger, "ASKAP linear (parallel) mosaic task " << ASKAP_PACKAGE_VERSION);
     ASKAPLOG_INFO_STR(logger, "Parset parameters:\n" << parset);
 
+    imagemath::LinmosAccumulator<float> accumulator;
+
+    // load the parset
+    if ( !accumulator.loadParset(parset) ) return;
+
+    // initialise an image accessor
+    accessors::IImageAccess& iacc = SynthesisParamsHelper::imageHandler();
+
+    // loop over the mosaics, reading each in an adding to the output pixel arrays
+    vector<string> inImgNames, inWgtNames, inSenNames;
+    string outImgName, outWgtName, outSenName;
+    map<string,string> outWgtNames = accumulator.outWgtNames();
+    for(map<string,string>::iterator ii=outWgtNames.begin(); ii!=outWgtNames.end(); ++ii) {
+
+        // get output files for this mosaic
+        outImgName = (*ii).first;
+        outWgtName = accumulator.outWgtNames()[outImgName];
+        ASKAPLOG_INFO_STR(logger, "++++++++++++++++++++++++++++++++++++++++++");
+        ASKAPLOG_INFO_STR(logger, "Preparing mosaic " << outImgName);
+        if (!accumulator.outWgtDuplicates()[outImgName]) {
+            ASKAPLOG_INFO_STR(logger, " - also weights image " << outWgtName);
+        }
+        accumulator.doSensitivity(false);
+        if (accumulator.genSensitivityImage()[outImgName]) {
+            outSenName = accumulator.outSenNames()[outImgName];
+            accumulator.doSensitivity(true);
+            ASKAPLOG_INFO_STR(logger, " - also sensitivity image " << outSenName);
+        }
+
+        // get input files for this mosaic
+        inImgNames = accumulator.inImgNameVecs()[outImgName];
+        ASKAPLOG_INFO_STR(logger, " - input images: "<<inImgNames);
+        if (accumulator.weightType() == FROM_WEIGHT_IMAGES) {
+            inWgtNames = accumulator.inWgtNameVecs()[outImgName];
+            ASKAPLOG_INFO_STR(logger, " - input weights images: " << inWgtNames);
+        }
+        else if (accumulator.weightType() == FROM_BP_MODEL) {
+            accumulator.beamCentres(loadBeamCentres(parset,iacc,inImgNames));
+        }
+        if (accumulator.doSensitivity()) {
+            inSenNames = accumulator.inSenNameVecs()[outImgName];
+            ASKAPLOG_INFO_STR(logger, " - input sensitivity images: " << inSenNames);
+        }
+
+    }
+
+    // set the output coordinate system and shape, based on the overlap of input images
+    vector<IPosition> inShapeVec;
+    vector<CoordinateSystem> inCoordSysVec;
+    for (vector<string>::iterator it = inImgNames.begin(); it != inImgNames.end(); ++it) {
+        casa::PagedImage<casa::Float> img(*it);
+        ASKAPCHECK(img.ok(),"Error loading "<< *it);
+        ASKAPCHECK(img.shape().nelements()>=3,"Work with at least 3D cubes!");
+        const casa::IPosition shape = img.shape();
+        ASKAPLOG_INFO_STR(logger," - Shape " << shape);
+        casa::IPosition blc(shape.nelements(),0);
+        casa::IPosition trc(shape);
+
+        blc[3] = comms.rank();
+        trc[3] = 1; // this could be nchan/nWorkers ...
+
+        ASKAPCHECK(blc[3]>=0 && blc[3]<shape[3], "Start channel is outside the number of channels or negative, shape: "<<shape);
+        ASKAPCHECK(trc[3]<=shape[3], "Subcube extends beyond the original cube, shape:"<<shape);
 
 
+        ASKAPLOG_INFO_STR(logger, " - Corners " << "blc  = " << blc << ", trc = " << trc << "\n");
+
+
+        casa::Slicer slc(blc,trc,casa::Slicer::endIsLength);
+        ASKAPLOG_INFO_STR(logger, " - Slicer " << slc);
+
+        casa::SubImage<casa::Float> si = casa::SubImage<casa::Float>(img,slc,casa::AxesSpecifier(casa::True));
+
+        // get the shape of a single channel slice based upon rank
+        inShapeVec.push_back(si.shape());
+        inCoordSysVec.push_back(si.coordinates());
+
+    }
+    accumulator.setOutputParameters(inShapeVec, inCoordSysVec);
+
+    // set up the output pixel arrays
+    Array<float> outPix(accumulator.outShape(),0.);
+    Array<float> outWgtPix(accumulator.outShape(),0.);
+    Array<float> outSenPix;
+    if (accumulator.doSensitivity()) {
+        outSenPix = Array<float>(accumulator.outShape(),0.);
+    }
+
+    // set up an indexing vector for the arrays
+    IPosition curpos(outPix.shape());
+    ASKAPASSERT(curpos.nelements()>=2);
+    for (uInt dim=0; dim<curpos.nelements(); ++dim) {
+        curpos[dim] = 0;
+    }
+
+    // loop over the input images, reading each in an adding to the output pixel arrays
+    for (uInt img = 0; img < inImgNames.size(); ++img ) {
+
+        // short cuts
+        string inImgName = inImgNames[img];
+        string inWgtName, inSenName;
+
+        ASKAPLOG_INFO_STR(logger, "Processing input image " << inImgName);
+        if (accumulator.weightType() == FROM_WEIGHT_IMAGES) {
+            inWgtName = inWgtNames[img];
+            ASKAPLOG_INFO_STR(logger, " - and input weight image " << inWgtName);
+        }
+        if (accumulator.doSensitivity()) {
+            inSenName = inSenNames[img];
+            ASKAPLOG_INFO_STR(logger, " - and input sensitivity image " << inSenName);
+        }
+
+        casa::PagedImage<casa::Float> inImg(inImgName);
+        const casa::IPosition shape = inImg.shape();
+        casa::IPosition blc(shape.nelements(),0);
+        casa::IPosition trc(shape);
+
+        blc[3] = comms.rank();
+        trc[3] = 1; // this could be nchan/nWorkers ...
+
+        casa::Slicer slc(blc,trc,casa::Slicer::endIsLength);
+
+        Array<float> inPix = inImg.getSlice(slc);
+
+        Array<float> inWgtPix;
+        Array<float> inSenPix;
+
+        if (accumulator.weightType() == FROM_WEIGHT_IMAGES) {
+
+            casa::PagedImage<casa::Float> inImg(inWgtName);
+            const casa::IPosition shape = inImg.shape();
+            casa::IPosition blc(shape.nelements(),0);
+            casa::IPosition trc(shape);
+
+            blc[3] = comms.rank();
+            trc[3] = 1; // this could be nchan/nWorkers ...
+
+            casa::Slicer slc(blc,trc,casa::Slicer::endIsLength);
+
+            inWgtPix = inImg.getSlice(slc);
+
+            ASKAPASSERT(inPix.shape() == inWgtPix.shape());
+        }
+        if (accumulator.doSensitivity()) {
+
+            casa::PagedImage<casa::Float> inImg(inSenName);
+            const casa::IPosition shape = inImg.shape();
+            casa::IPosition blc(shape.nelements(),0);
+            casa::IPosition trc(shape);
+
+            blc[3] = comms.rank();
+            trc[3] = 1; // this could be nchan/nWorkers ...
+
+            casa::Slicer slc(blc,trc,casa::Slicer::endIsLength);
+
+            inSenPix = inImg.getSlice(slc);
+
+            ASKAPASSERT(inPix.shape() == inSenPix.shape());
+        }
+    }
+    // set up an iterator for all directionCoordinate planes in the input image
+    scimath::MultiDimArrayPlaneIter planeIter(accumulator.inShape());
+
+    // test whether to simply add weighted pixels, or whether a regrid is required
+    bool regridRequired = !accumulator.coordinatesAreEqual();
+
+    // if regridding is required, set up buffer some images
+    if ( regridRequired ) {
+
+        ASKAPLOG_INFO_STR(logger, " - regridding -- input pixel grid is different from the output");
+
+    }
 }
 class linmosMPIApp : public askap::Application
 {
@@ -35,11 +212,14 @@ class linmosMPIApp : public askap::Application
 
         virtual int run(int argc, char* argv[]) {
 
+            // This class must have scope outside the main try/catch block
+            askap::askapparallel::AskapParallel comms(argc, const_cast<const char**>(argv));
+
             try {
                 StatReporter stats;
                 LOFAR::ParameterSet subset(config().makeSubset("linmos."));
                 SynthesisParamsHelper::setUpImageHandler(subset);
-                mergeMPI(subset);
+                mergeMPI(subset, comms);
                 stats.logSummary();
                 return 0;
             }
--- Code/Components/Synthesis/synthesis/current/apps/linmos.cc
+++ Code/Components/Synthesis/synthesis/current/apps/linmos.cc
@@ -2,7 +2,7 @@
 ///
 /// @brief combine a number of images as a linear mosaic
 /// @details This is a utility to merge images into a mosaic. Images can be set
-/// explicitly or found automatically based on input tags. 
+/// explicitly or found automatically based on input tags.
 ///
 /// @copyright (c) 2012,2014,2015 CSIRO
 /// Australia Telescope National Facility (ATNF)
@@ -49,6 +49,7 @@
 // Local packages includes
 #include <measurementequation/SynthesisParamsHelper.h>
 #include <linmos/LinmosAccumulator.h>
+#include <utils/LinmosUtils.h>
 
 ASKAP_LOGGER(logger, ".linmos");
 
@@ -56,122 +57,6 @@ using namespace casa;
 using namespace askap;
 using namespace askap::synthesis;
 
-/// @brief helper method to load beam offsets from the parset file
-/// @details shares the same format as csimulator feed definition. This is needed to support ASKAP BETA,
-///    which initially uses the same image centre for all beams, leaving beam offsets unspecified.
-///    Therefore, this information has to be supplied by other means. Copied from testlinmos.
-/// @param[in] const LOFAR::ParameterSet &parset : parset containing spacing and offset parameters
-/// @param[in] const Vector<std::string> beamNames : which offsets to get from the parset
-/// @param[in] MVDirection centre : the pointing centre, which all offsets are relative to
-/// @return Vector<MVDirection> : a MVDirection for each name in beamNames
-Vector<MVDirection> loadBeamOffsets(const LOFAR::ParameterSet &parset,
-                                    const Vector<std::string> beamNames,
-                                    MVDirection centre) {
-
-    Vector<MVDirection> centres (beamNames.size(), centre);
-
-    ASKAPLOG_INFO_STR(linmoslogger, " -> looking for the feed spacing");
-    Quantity qspacing = askap::asQuantity(parset.getString("feeds.spacing"));
-    double spacing = qspacing.getValue("rad");
-    ASKAPLOG_INFO_STR(linmoslogger, "    beam spacing set to " << qspacing);       
-
-    ASKAPLOG_INFO_STR(linmoslogger, " -> looking for a feed offset for each image");
-    for (uint beam = 0; beam < beamNames.size(); ++beam) {
-         const string parName = "feeds." + beamNames[beam];
-         const Vector<double> xy(parset.getDoubleVector(parName));
-         //ASKAPCHECK(xy.size() == 2, "Expect two elements for each offset");
-         // the shift appears to be positive in HA, so multiply by -1. Simulator.cc states:
-         // "x direction is flipped to convert az-el type frame to ra-dec"
-         centres[beam].shift(-xy[0]*spacing, xy[1]*spacing, casa::True);
-         ASKAPLOG_INFO_STR(linmoslogger, " -> " << parName << " centre: " << centres[beam] );
-    }
-    return centres;
-}
-
-/// @brief helper method to get beam centres from parset and/or image metadata
-/// @details separate from loadParset to allow metadata to be read from input images
-/// @param[in] const LOFAR::ParameterSet &parset: linmos parset
-/// @param[in] const accessors::IImageAccess &iacc: image accessor
-/// @param[in] const string outImgName: current mosaic name
-/// @return bool true=success, false=fail
-Vector<MVDirection> loadBeamCentres(const LOFAR::ParameterSet &parset,
-                                    const accessors::IImageAccess &iacc,
-                                    const vector<string> &inImgNames) {
-
-    // if setting weights using beam models, check the input for extra information
-
-    ASKAPLOG_INFO_STR(linmoslogger, "Looking for parset options associated with primary-beam models");
-
-    MVDirection centre;
-    bool centreDefined = false;
-
-    // set the centre of the "feeds" offset parameters (e.g. the boresight of the PAF)
-    if (parset.isDefined("feeds.centre")) {
-        ASKAPLOG_INFO_STR(linmoslogger, "Found centre of the feeds to use in beam models:");
-        const vector<string> feedsCentre(parset.getStringVector("feeds.centre"));
-        ASKAPCHECK(feedsCentre.size()==2, " -> the feeds.centre vector should have 2 elements");
-        centre = convertDir(feedsCentre[0], feedsCentre[1]);
-        ASKAPLOG_INFO_STR(linmoslogger, " -> "<<feedsCentre<<", = "<<centre);
-        centreDefined = true;
-    }
-    else if (parset.isDefined("feeds.centreref")) {
-        uint centreref = parset.getInt("feeds.centreref");
-        if (centreref<inImgNames.size()) {
-            ASKAPLOG_INFO_STR(linmoslogger, "Using the reference pixel of input image "<<centreref<<
-                " as the centre of the feeds to use in beam models");
-            const CoordinateSystem coordSys = iacc.coordSys(inImgNames[centreref]);
-            const int DCpos = coordSys.findCoordinate(Coordinate::DIRECTION,-1);
-            const DirectionCoordinate DC = coordSys.directionCoordinate(DCpos);
-            DC.toWorld(centre,DC.referencePixel());
-            ASKAPLOG_INFO_STR(linmoslogger, " -> "<<centre);
-            centreDefined = true;
-        }
-        else {
-            ASKAPLOG_WARN_STR(linmoslogger, "Found unsuitable centreref parameter: "<<centreref);
-        }
-    }
-
-    // centres for each beam
-    if (centreDefined) {
-
-        if (parset.isDefined("feeds.offsetsfile")) {
-
-            ASKAPLOG_INFO_STR(linmoslogger,  "Loading beam offsets from " << parset.getString("feeds.offsetsfile"));
-            LOFAR::ParameterSet feed_parset(parset.getString("feeds.offsetsfile"));
-
-            vector<string> beamNames;
-            ASKAPLOG_INFO_STR(linmoslogger, " -> looking for feed names");
-            if (parset.isDefined("feeds.names")) {
-                beamNames = parset.getStringVector("feeds.names", true);
-                ASKAPLOG_INFO_STR(linmoslogger,  "    using names given in the main parset");
-            } else if (feed_parset.isDefined("feeds.names")) {
-                beamNames = feed_parset.getStringVector("feeds.names", true);
-                ASKAPLOG_INFO_STR(linmoslogger,  "    using names given in the feed-offset parset");
-            }
-            ASKAPCHECK(beamNames.size() > 0, "No beams specified");
-            ASKAPCHECK(beamNames.size() == inImgNames.size(),
-               "Number of beams does not match number of input files");
-
-            return loadBeamOffsets(feed_parset, beamNames, centre);
-
-            if (parset.isDefined("feeds.spacing")) {
-                ASKAPLOG_WARN_STR(linmoslogger, "Feed info specified in parset but ignored. Using offset file");
-            }
-
-        } else {
-
-            ASKAPCHECK(parset.isDefined("names"), "No names specified in parset");
-            return loadBeamOffsets(parset, parset.getStringVector("names", true), centre);
-
-        }
-
-    } else {
-        ASKAPLOG_WARN_STR(linmoslogger, "Centre of the feeds not found. Setting beam centres to input ref. pixels");
-    }
-
-    return Vector<MVDirection>();
-
-}
 
 /// @brief do the merge
 /// @param[in] parset subset with parameters
@@ -378,7 +263,7 @@ static void merge(const LOFAR::ParameterSet &parset) {
         iacc.create(outImgName, accumulator.outShape(), accumulator.outCoordSys());
         iacc.write(outImgName,outPix);
         iacc.setUnits(outImgName,units);
-        if (psf.nelements()>=3) 
+        if (psf.nelements()>=3)
             iacc.setBeamInfo(outImgName, psf[0].getValue("rad"), psf[1].getValue("rad"), psf[2].getValue("rad"));
 
         if (accumulator.outWgtDuplicates()[outImgName]) {
@@ -388,7 +273,7 @@ static void merge(const LOFAR::ParameterSet &parset) {
             iacc.create(outWgtName, accumulator.outShape(), accumulator.outCoordSys());
             iacc.write(outWgtName,outWgtPix);
             iacc.setUnits(outWgtName,units);
-            if (psf.nelements()>=3) 
+            if (psf.nelements()>=3)
                 iacc.setBeamInfo(outWgtName, psf[0].getValue("rad"), psf[1].getValue("rad"), psf[2].getValue("rad"));
         }
 
@@ -397,7 +282,7 @@ static void merge(const LOFAR::ParameterSet &parset) {
             iacc.create(outSenName, accumulator.outShape(), accumulator.outCoordSys());
             iacc.write(outSenName,outSenPix);
             iacc.setUnits(outSenName,units);
-            if (psf.nelements()>=3) 
+            if (psf.nelements()>=3)
                 iacc.setBeamInfo(outSenName, psf[0].getValue("rad"), psf[1].getValue("rad"), psf[2].getValue("rad"));
         }
 
