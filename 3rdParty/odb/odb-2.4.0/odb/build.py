d704e79bd4c38cea42445ef0d2f79ec427983f49
DanielCollins <danielcollins@csiro.au>
2016-10-07 08:05:57 +0000

    ASKAPSDP-2211 fixed internal compiler error in odb by dropping back to O1 optimisations

--- 3rdParty/odb/odb-2.4.0/odb/build.py
+++ 3rdParty/odb/odb-2.4.0/odb/build.py
@@ -4,12 +4,12 @@ from askapdev.rbuild.builders import Autotools as Builder
 
 builder = Builder(pkgname="odb-2.4.0")
 
-# setup the libodb dependency
-# libodb = builder.dep.get_install_path("libodb")
-# builder.add_option("CPPFLAGS=-I{0}/include".format(libodb))
-# builder.add_option("LDFLAGS=-L{0}/lib".format(libodb))
-# --with-libodb isn't working, but CPPFLAGS and LDFLAGS are OK
-# builder.add_option("--with-libodb={0}".format(libodb))
+# cutl dependency
+libcutl = builder.dep.get_install_path("libcutl")
+builder.add_option("CPPFLAGS=-I{0}/include".format(libcutl))
+builder.add_option("LDFLAGS=-L{0}/lib".format(libcutl))
+
+builder.add_option("CXXFLAGS=-O1")
 
 builder.remote_archive = "odb-2.4.0.tar.gz"
 builder.nowarnings = True
