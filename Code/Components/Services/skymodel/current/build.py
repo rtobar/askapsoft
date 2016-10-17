cd908432495ad878f1609ef33e7c47d07cf2ca89
DanielCollins <danielcollins@csiro.au>
2016-10-17 07:35:20 +0000

    ASKAPSDP-2238 moving ODB files to support rbuild cleaning

--- Code/Components/Services/skymodel/current/build.py
+++ Code/Components/Services/skymodel/current/build.py
@@ -20,7 +20,8 @@ def odb_prebuild():
         os.path.expandvars("$ASKAP_ROOT"),
         "3rdParty/odb/odb-2.4.0/odb/install/bin/odb")
 
-    datamodel_dir = os.path.abspath("./datamodel")
+    schema_dir = os.path.abspath("./schema")
+    output_dir = os.path.abspath("./datamodel")
 
     odb_includes = os.path.join(
         os.path.expandvars("$ASKAP_ROOT"),
@@ -31,20 +32,20 @@ def odb_prebuild():
         odb_compiler,  # The ODB compiler
         '--generate-query',  # Generate query support code
         '--generate-schema',  # Generate database schema SQL
-        '--output-dir', datamodel_dir,  # output location for generated files
+        '--output-dir', output_dir,  # output location for generated files
         '--database', database,  # Target database. Using SQLite for now, but will need to switch to a dynamic option later
         '-I', odb_includes,  # ODB headers
         # '',  #
         ]
 
     # append the list of sources
-    sources = glob.glob(os.path.join(datamodel_dir, "*.h"))
+    sources = glob.glob(os.path.join(schema_dir, "*.h"))
     cmd.extend(sources)
 
     # I want a failed ODB compile to abort the whole build. Using check_call
     # with the raised exception on failure seems the simplest way to achieve
     # this.
-    print(cmd)
+    # print(cmd)
     return subprocess.check_call(cmd)
     # return runcmd(cmd)[2]
 
