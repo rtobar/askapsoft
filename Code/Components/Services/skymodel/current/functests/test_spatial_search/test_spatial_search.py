8ff46fdb6a3a5bdcf309956ef50219cdcb1d7ae5
DanielCollins <danielcollins@csiro.au>
2017-04-03 08:59:02 +0000

    ASKAPSDP-2526 extended functional test to verify the data marshalling function

--- Code/Components/Services/skymodel/current/functests/test_spatial_search/test_spatial_search.py
+++ Code/Components/Services/skymodel/current/functests/test_spatial_search/test_spatial_search.py
@@ -13,6 +13,7 @@ import sys
 from datetime import datetime
 from time import sleep
 
+import nose.tools as nt
 from unittest import skip
 
 from askap.iceutils import CPFuncTestBase, get_service_object
@@ -53,8 +54,33 @@ class Test(CPFuncTestBase):
         components = self.sms_client.coneSearch(
             centre=Coordinate(70.2, -61.8),
             radius=1.0)
+
         assert len(components) == 1
-        assert components[0].componentId == "SB1958_image.i.LMC.cont.sb1958.taylor.0.restored_1a"
+        c = components[0]
+        assert c.componentId == "SB1958_image.i.LMC.cont.sb1958.taylor.0.restored_1a"
+        nt.assert_almost_equal(c.ra, 70.176918)
+        nt.assert_almost_equal(c.dec, -61.819671)
+        nt.assert_almost_equal(c.raErr, 0.00999999977)
+        nt.assert_almost_equal(c.decErr, 0.00999999977)
+        nt.assert_almost_equal(c.freq, 1400.5)
+        nt.assert_almost_equal(c.fluxPeak, 326.529998779)
+        nt.assert_almost_equal(c.fluxPeakErr, 0.282999992371)
+        nt.assert_almost_equal(c.fluxInt, 378.830993652)
+        nt.assert_almost_equal(c.fluxIntErr, 0.541999995708)
+        nt.assert_almost_equal(c.spectralIndex, -1.24000000954)
+        nt.assert_almost_equal(c.spectralCurvature, -1.37999999523)
+
+        # this database has polarisation data
+        assert len(c.polarisation) == 1
+        p = c.polarisation[0]
+        nt.assert_almost_equal(p.lambdaRefSq, 0.09)
+        nt.assert_almost_equal(p.polPeakFit, 1.55)
+        nt.assert_almost_equal(p.polPeakFitDebias, 1.42)
+        nt.assert_almost_equal(p.polPeakFitErr, 0.01)
+        nt.assert_almost_equal(p.polPeakFitSnr, 150.0)
+        nt.assert_almost_equal(p.polPeakFitSnrErr, 1.0)
+        nt.assert_almost_equal(p.fdPeakFit, 52.0)
+        nt.assert_almost_equal(p.fdPeakFitErr, 5.2)
 
     def test_simple_rect_search(self):
         roi = Rect(centre=Coordinate(79.375, -71.5), extents=RectExtents(0.75, 1.0))
