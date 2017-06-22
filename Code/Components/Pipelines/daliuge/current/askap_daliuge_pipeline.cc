d6011c41bae7d67a0b2b874d3aee5a2daaef415b
Ord, Stephen (CASS, Marsfield) <Stephen.Ord@csiro.au>
2017-06-22 17:17:29 +1000

    added the daliuge pipeline but it needs cleaning up

---
+++ Code/Components/Pipelines/daliuge/current/askap_daliuge_pipeline.cc
@@ -0,0 +1,17 @@
+// This is an automatically generated file. Please DO NOT edit!
+/// @file
+///
+/// Package config file.
+
+#include "askap_daliuge_pipeline.h"
+
+/// askap namespace
+namespace askap {
+
+  /// @return version of the package
+  std::string getAskapPackageVersion_daliuge_pipeline() {
+      return std::string("daliuge; ASKAPsoft==features/ASKAPSDP-2465-B; runknown; 2017-06-22");
+  }
+
+}
+
